2dbc597e0c9f0fdd9e7d0a00d0b6038dded78eef
Dhananjay Waikar <dwaikar28041994@gmail.com>
2022-05-11 09:21:52 +0530

    New Windows Form App Added

---
+++ WindowsFormAssignment/WindowsFormAssignment/CalculationForm.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CalculationForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.txtWidth = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtBreadth = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(75, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Height"
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(198, 57)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(100, 23)
+        Me.txtHeight.TabIndex = 1
+        '
+        'txtWidth
+        '
+        Me.txtWidth.Location = New System.Drawing.Point(198, 102)
+        Me.txtWidth.Name = "txtWidth"
+        Me.txtWidth.Size = New System.Drawing.Size(100, 23)
+        Me.txtWidth.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(75, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Width"
+        '
+        'txtBreadth
+        '
+        Me.txtBreadth.Location = New System.Drawing.Point(198, 151)
+        Me.txtBreadth.Name = "txtBreadth"
+        Me.txtBreadth.Size = New System.Drawing.Size(100, 23)
+        Me.txtBreadth.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(75, 154)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Breadth"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(28, 6)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(132, 32)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Calculation"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(132, 209)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CalculationForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtBreadth)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtWidth)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "CalculationForm"
+        Me.Text = "Calculation"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents txtWidth As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtBreadth As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ WindowsFormAssignment/WindowsFormAssignment/CalculationForm.vb
@@ -0,0 +1,6 @@
+﻿Public Class CalculationForm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Decimal = Convert.ToDecimal(txtBreadth.Text) * Convert.ToDecimal(txtHeight.Text) * Convert.ToDecimal(txtWidth.Text)
+        MessageBox.Show("Total : " + Convert.ToString(i))
+    End Sub
+End Class
